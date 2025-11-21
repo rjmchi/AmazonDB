6cf4cd86b127fe4e3dc7a270d8534eaec3b51699
rjmchi <robert@rjmprogramming.com>
2025-11-21 13:22:29 -0600

    fixes image on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
 
     <div class="py-3">
 
-        <div class="m-4 p-5 border rounded-lg bg-white">
+        <div class="m-4 p-5 border rounded-lg bg-white grid grid-cols-2">
             @foreach ($books as $book)
                 <div class="mb-5 p-4 border rounded bg-neutral-100">
                     <a href="{{ $book->link }}" class="flex">
-                        <img class="w-40" src="{{ asset('/images/' . $book->image) }}">
+                        <img class="h-50" src="{{ asset('/images/' . $book->image) }}">
                         <div class="ml-4">
                             <p class="text-2xl font-medium">{{ $book->title }}</p>
                             <p class="mt-3">{{ $book->description }}</p>
